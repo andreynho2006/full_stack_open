9a30988b5b1dabfa045aace6ce789d27bdba29d7
andreynho2006 <andreynho2006@gmail.com>
2019-09-24 22:50:54 +0300

    commit step 1.7

--- part1/step6/src/index.js
+++ part1/step6/src/index.js
@@ -12,6 +12,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = (good + neutral + bad)
+  const average = isNaN((good * 1 + bad * (-1))/all) ? 0 :(good * 1 + bad * (-1))/all;
+  const positive = isNaN(good / all)*100 ? 0 : (good / all)*100;
 
   return (
     <div>
@@ -23,6 +26,9 @@ const App = () => {
         <p>good: {good}</p>
         <p>neutral: {neutral}</p>
         <p>bad: {bad}</p>
+        <p>all: {all}</p>
+        <p>average: {average}</p>
+        <p>positive: {positive} %</p>
     </div>
   )
 }
