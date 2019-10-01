67714849ff71372fcd74586b60f942c6e904b4f0
andreynho2006 <andreynho2006@gmail.com>
2019-10-01 14:12:14 +0300

    refactor  part2 exercise 2.4 and 2.5

--- part2/2.1/src/components/Part.js
+++ part2/2.1/src/components/Part.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Part = (props) => (
-    <p>Part: {props.part} : {props.exercises} exercises</p>
+    <p>Part: {props.part} : {props.exercises}</p>
 )
 
 export default Part
